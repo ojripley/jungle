6286c524ed459596974e94450b63f5290d217729
Owen Ripley <ojripley@gmail.com>
2019-11-13 09:51:12 -0500

    inventory counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+
   def show
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
   end
+
 end
