ed6bd7afbe6fa785d388499bbfa2e60e811453d7
Owen Ripley <ojripley@gmail.com>
2019-11-13 15:25:29 -0500

    add user login routes, controller

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,12 @@ Rails.application.routes.draw do
 
   get '/about' => 'about#show'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # namespace :about do
   #   root to: 'about#show'
